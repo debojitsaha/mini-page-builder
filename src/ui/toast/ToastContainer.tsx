df769797e38794e91c1ec5c900c79b5e9ea3d183
debojitsaha <saha05debojit@gmail.com>
2024-07-27 13:20:55 +0530

    update: toast position, modal close, type def and minor changes

--- src/ui/toast/ToastContainer.tsx
+++ src/ui/toast/ToastContainer.tsx
@@ -7,7 +7,7 @@ interface ToastContainerProps {
 
 export default function ToastContainer({ toasts }: ToastContainerProps) {
   return (
-    <div className="fixed top-0 left-1/2 z-50 flex flex-col items-center p-4 space-y-2">
+    <div className="w-full fixed top-0 z-50 flex flex-col items-center p-4 space-y-2">
       {toasts.map((toast) => (
         <Toast key={toast.id} message={toast.message} type={toast.type} />
       ))}
